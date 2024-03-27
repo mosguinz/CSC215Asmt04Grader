9706c8edda04abffb2929b5a7aa80816fa528b56
mosguinz <mos.guinz@gmail.com>
2024-03-27 01:35:45 -0700

    Add grep to look for array declaration and class name

--- run_pb.sh
+++ run_pb.sh
@@ -1,1 +1,6 @@
 java *[bB]/*.java < pb_stdin.txt
+echo "\n\n================================================"
+echo "\nLooking for \`Object[4][3]\`"
+cat *[bB]/*.java | grep -n "Object\[4\]\[3\]" 
+echo "\nLooking for \`.class.getSimpleName\`"
+cat *[bB]/*.java | grep -n "class\.getSimpleName"
