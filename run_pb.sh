641921e5254314443c3631d19722bb0b5712b646
mosguinz <mos.guinz@gmail.com>
2024-03-27 17:52:18 -0700

    Fix check for `getSimpleName`

--- run_pb.sh
+++ run_pb.sh
@@ -2,6 +2,6 @@ java *[bB]/*.java < pb_stdin.txt > a.txt
 vimdiff a.txt pb_stdout.txt
 rm a.txt
 echo "\nLooking for \`Object[4][3]\`"
-cat *[bB]/*.java | grep -n "Object\[4\]\[3\]" 
-echo "\nLooking for \`.class.getSimpleName\`"
-cat *[bB]/*.java | grep -n "class\.getSimpleName"
+grep -n "Object\[4\]\[3\]" *[bB]/*.java
+echo "\nLooking for \`.class.getSimpleName\` or \`.getClass().getSimpleName()\`"
+grep -n "[cC]lass.*getSimpleName" *[bB]/*.java
